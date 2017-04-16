34dff21a45f35e02a1a299c95706320e1684eb66
atknin <atknini@yahoo.com>
2017-04-16 14:33:09 +0300

    1

--- github-pull.php
+++ github-pull.php
@@ -1,5 +1,5 @@
 <?php
-$output = shell_exec('cd ~scsciru1/www/ && git pull');
+$output = shell_exec('eval `ssh-agent -s` && ssh-add ~scsciru1/.ssh/github && cd ~scsciru1/www/ && git pull');
 echo "<pre>$output</pre>";
 ?>
 
