3427abe3bbc0ff553d328d109fe99bb52db66e7e
atknin <atknini@yahoo.com>
2017-04-16 13:51:09 +0300

    ok

--- github-pull.php
+++ github-pull.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd cd ~scsciru1/www/ && git pull origin master' );
+  shell_exec( 'cd ~scsciru1/www/ && git pull' );
 }
 ?>hi
