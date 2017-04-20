2affc6d5acab6a958bf23d6f11aa004f1a2ff6dd
atknin <atknini@yahoo.com>
2017-04-20 03:25:03 +0300

    123

--- characters.php
+++ characters.php
@@ -15,92 +15,88 @@ $tasktosql = 'select * from comand';
 $taskto = mysqli_query($link, $tasktosql);
 ?>
 <div class="characters">
-<div class="header">
-<div class="text">
-	На видео справа наша<br>
-	лаборатория,мы ведем <br>
-	съемку в режиме реального <br>
-	времени
-</div>
-<div class="video">
-<div style="text-align: center; color: white; font-size: 26pt; margin-top: 80px;";>
-ВИДЕО
-</div>
-</div>
-</div>
-<div class="maincontent">
-<table>
-<tbody>
-<?php
-$numb = 0;
-while($row = mysqli_fetch_array($taskto)) {
-	$ost = $numb % 2;
-	if($ost == 0){
-		echo "<tr>";
-	}
-
-	$row['task'] = wordwrap($row['task'], 150);
-	$row['nametask'] = wordwrap($row['nametask'], 150);
-
-echo '
-<td>
-	<div class="guy">
-		<div class="avatar">
-			<img src="'. $row['photo'] .'">
-		</div>
-
-		<div class="info">
-			<div class="name">'
-				.$row['name'].
-			'</div>
-			
-			<div class="bl1">
-			</div>
-			
-			<div class="infoguy">
-
-				<div class="you">
-					О себе:
-				</div>
-
-				<div class="youyou">'
-					.$row['task'].
-				'</div>
-
-				<div class="work">
-					Название работы:
-				</div>
-		
-				<div class="workyou">'
-					.$row['nametask'].
-				'</div>
-		
-				<div class="mailyou">'
-					.$row['contact'].
-				'</div>
-
-				<a href="mailto:'.$row['contact'].'?Subject='.$row['name'].'" target="_top">
-					<img src="logo/mail_sent.png">
-				</a>
-	
-				<div class="bl2mail">
-				</div>
-
-			</div>
+	<div class="header"> </div>
+	<div class="text">
+		На видео справа наша<br>
+		лаборатория,мы ведем <br>
+		съемку в режиме реального <br>
+		времени
+	</div>
+	<div class="video">
+		<div style="text-align: center; color: white; font-size: 26pt; margin-top: 80px;";>
+		ВИДЕО
 		</div>
 	</div>
-</td>';
-$numb+= 1;
-$ost = $numb % 2;
-if($ost == 0){
-echo "</tr>";
-}
-}
-?>
-</tbody>
-</table>
 </div>
+
+<div class="row">
+	<?php
+	$numb = 0;
+	while($row = mysqli_fetch_array($taskto)) {
+		$ost = $numb % 2;
+		if($ost == 0){
+			echo "<tr>";
+		}
+
+		$row['task'] = wordwrap($row['task'], 150);
+		$row['nametask'] = wordwrap($row['nametask'], 150);
+
+	echo '
+		<div class="guy col-md-5 col-lg-5">
+			<div class="avatar">
+				<img src="'. $row['photo'] .'">
+			</div>
+
+			<div class="info">
+				<div class="name">'
+					.$row['name'].
+				'</div>
+				
+				<div class="bl1">
+				</div>
+				
+				<div class="infoguy">
+
+					<div class="you">
+						О себе:
+					</div>
+
+					<div class="youyou">'
+						.$row['task'].
+					'</div>
+
+					<div class="work">
+						Название работы:
+					</div>
+			
+					<div class="workyou">'
+						.$row['nametask'].
+					'</div>
+			
+					<div class="mailyou">'
+						.$row['contact'].
+					'</div>
+
+					<a href="mailto:'.$row['contact'].'?Subject='.$row['name'].'" target="_top">
+						<img src="logo/mail_sent.png">
+					</a>
+		
+					<div class="bl2mail">
+					</div>
+
+				</div>
+			</div>
+		</div>';
+	$numb+= 1;
+	$ost = $numb % 2;
+	if($ost == 0){
+	echo;
+	}
+	}
+	?>
 </div>
+
+
 <?php include("footer.php");?>
 </body>
 </html>
